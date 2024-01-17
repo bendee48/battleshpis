07a5795a51fc896fbbc4ad18ce4a92ce9177ac34
bendee <benbanj@hotmail.com>
2024-01-17 17:53:28 +0000

    Implementing Ship class with tests
    - Adding Ship class
    - Adding tests and implementing .length and .hits properties
    - Adding hit() function with tests
    - Adding isSunk() function with tests

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,23 @@
 class Ship {
-  constructor() {
-    this.length = 4;
+  constructor(length) {
+    this._length = length;
+    this._hits = 0;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get hits() {
+    return this._hits;
+  }
+
+  hit() {
+    this._hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
   }
 }
 
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -2,11 +2,55 @@ import Ship from 'ship';
 
 // Ship Class
 describe('Ship Class', () => {
-  let ship = new Ship();
+  const battleship = new Ship(4);
+  const submarine = new Ship(3);
+  const patrolBoat = new Ship(2);
 
-  describe('length property', () => {
-    it('returns the length of the ship', () => {
-      expect(ship.length).toBe(4);
+  describe('.length', () => {
+    it('returns the length of a battleship', () => {
+      expect(battleship.length).toBe(4);
+    })
+
+    it('returns the length of a submarine', () => {
+      expect(submarine.length).toBe(3);
+    });
+
+    it('returns the length of a submarine', () => {
+      expect(patrolBoat.length).toBe(2);
+    });
+  });
+
+  describe('.hits', () => {
+    it('returns number of hits to ship', () => {
+      expect(battleship.hits).toBe(0);
+    });
+  })
+
+  describe('hit()', () => {
+    it('increases the number of hits a ship has suffered by one', () => {
+      const ship = new Ship();
+      ship.hit();
+      expect(ship.hits).toBe(1);
+    })
+
+    it('increases the total no. of hits after multiple blows', () => {
+      const ship = new Ship();
+      for (let i = 0; i < 3; i++) ship.hit();
+      expect(ship.hits).toBe(3);
+    })
+  });
+
+  describe('isSunk()', () => {
+    it('returns true if ship has been hit more times than it\'s length', () => {
+      const submarine = new Ship(3);
+      for (let i = 0; i < 3; i++) submarine.hit();
+      expect(submarine.isSunk()).toBe(true);
+    })
+
+    it('returns false if ship hasn\'t been hit more times than it\'s length', () => {
+      const submarine = new Ship(3);
+      for (let i = 0; i < 2; i++) submarine.hit();
+      expect(submarine.isSunk()).toBe(false);
     })
   });
 });
