3a6db0aef171aa5028ce3f453332c9bdbc6b4e32
bendee <benbanj@hotmail.com>
2024-02-06 18:20:56 +0000

    Adding tests and functionality for Gameboard class
    - Adding tests for a createBoard() function
    - Adding the functions to Gameboard class to makes tests pass
    - Adding jest-environment-jsdom to dev dependencies to be able to run tests on DOM elements

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,9 +1,60 @@
 class Gameboard {
   constructor() {
+    this.board = this.createBoard();
     this.cells = { a1: {className: 'battleship'} };
   }
 
-  placeShip() {}
+  placeShip(coord, ship) {
+
+  }
+
+  /**
+   * Creates a game board DOM element with a 10x10 grid, made up of individual cells.
+   *
+   * @returns {HTMLElement} The generated game board element.
+   */
+  createBoard() {
+    const board = document.createElement('div');
+    board.classList.add('board');
+
+    for (let i = 1; i < 11; i++) {
+      for (let j = 0; j < 10; j++) {
+        const coord = this.#createCoordinate(i, j);
+        board.appendChild(this.#createCell(coord));
+      }
+    }
+
+    return board;
+  }
+
+  /**
+   * Creates a coordinate.
+   * 
+   * @private
+   * @param {number} number - Numerical part of coordinate.
+   * @param {number} letterNum - An index for a letter.
+   * @returns {String} The generated coordinate eg 'J4'.
+   */
+  #createCoordinate(number, letterNum) {
+    const letters = 'ABCDEFGHIJ';
+    return letters[letterNum] + number;
+  }
+
+  /**
+   * Creates an individual cell for the game board.
+   * 
+   * @private
+   * @param {string} coord - The coordinate of the cell, e.g., 'A1'.
+   * @returns {HTMLElement} The generated cell element.
+   */
+  #createCell(coord) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    // Add data-set attribute eg. data-coordinate='A1'
+    cell.dataset.coordinate = coord;
+
+    return cell;
+  }
 }
 
 export default Gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,15 +1,40 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import Gameboard from 'gameboard';
 
 // Gameboard Class
 describe('Gameboard class', () => {
   const gameboard = new Gameboard();
 
+  describe('.createBoard()', () => {
+    it('creates a board', () => {
+      expect(gameboard.board.tagName).toBe('DIV');
+      expect(gameboard.board.className).toBe('board');
+    })
+
+    it('creates a board with 100 cells', () => {
+      expect(gameboard.board.childElementCount).toBe(100);
+    })
+
+    it('labels each cell with a coordinate', () => {
+      expect(gameboard.board.childNodes[0].dataset.coordinate).toBe('A1');
+      expect(gameboard.board.childNodes[9].dataset.coordinate).toBe('J1');
+      expect(gameboard.board.childNodes[44].dataset.coordinate).toBe('E5');
+      expect(gameboard.board.childNodes[76].dataset.coordinate).toBe('G8');
+      expect(gameboard.board.childNodes[99].dataset.coordinate).toBe('J10');
+    })
+  })
+
   describe('.placeShip()', () => {
     it('places a ship on the board', () => {
       let battleship = jest.mock('ship'); // battleship is 4 length
       gameboard.placeShip('a1', battleship);
       expect(gameboard.cells['a1'].className.includes('battleship')).toBe(true);
-      // add remaining ships coords to test
+      expect(gameboard.cells['a2'].className.includes('battleship')).toBe(true);
+      // expect(gameboard.cells['a3'].className.includes('battleship')).toBe(true);
+      // expect(gameboard.cells['a4'].className.includes('battleship')).toBe(true);
     })
   })
 
