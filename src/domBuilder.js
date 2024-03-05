40c6278d866a8845f5d3d24832c183f15761998d
bendee <benbanj@hotmail.com>
2024-03-05 21:19:56 +0000

    Adding documentation for the DOM builder and DOM controller modules

--- src/domBuilder.js
+++ src/domBuilder.js
@@ -1,4 +1,17 @@
+/**
+ * Module for building HTML elements for the DOM.
+ *
+ * @returns {Object} The public API with the buildDraggable function.
+ */
 const DOMBuilder = (() => {
+  /**
+   * Builds a draggable element with cells.
+   *
+   * @param {string} name - The name of the draggable element.
+   * @param {number} length - The number of cells to be appended.
+   * @returns {HTMLElement} The draggable element.
+   * @example - const draggableElement = DOMBuilder.buildDraggable('example', 3);
+   */
   const buildDraggable = (name, length) => {
     const element = document.createElement('div');
     element.setAttribute('id', `${name}-draggable`);
@@ -11,12 +24,27 @@ const DOMBuilder = (() => {
     return element;
   }
 
+  /**
+   * Appends cells to the draggable element.
+   *
+   * @private
+   * @param {HTMLElement} element - The draggable element.
+   * @param {string} name - The name of the draggable element.
+   * @param {number} length - The number of cells to be appended.
+   */
   const _appendCells = (element, name, length) => {
     for (let i = 0; i < length; i++) {
       element.appendChild(_buildDraggableCell(name));
     }
   }
 
+  /**
+   * Builds a draggable cell element.
+   *
+   * @private
+   * @param {string} name - The name of the draggable element.
+   * @returns {HTMLElement} The draggable cell element.
+   */
   const _buildDraggableCell = (name) => {
     const element = document.createElement('div');
     element.classList.add('cell', name);
@@ -24,6 +52,11 @@ const DOMBuilder = (() => {
     return element;
   }
 
+  /**
+   * Public API of the module.
+   * 
+   * @returns {Object} The public API with the buildDraggable function.
+   */
   return { buildDraggable };
 })();
 
--- src/domController.js
+++ src/domController.js
@@ -1,21 +1,50 @@
 import DOMBuilder from "./domBuilder";
 import Ship from './ship';
 
+/**
+ * Module for displaying HTML elements of the DOM.
+ *
+ * @returns {Object} The public API with the displayBoard, displayDraggables functions.
+ */
 const DOMController = (() => {
+  // container for the game boards
   const boardsContainer = document.querySelector('.boards-container');
+  // container for the draggable elements
   const draggablesContainer = document.querySelector('.draggables-container');
 
+  /**
+   * Displays a game board.
+   *
+   * @param {object} gameboard - A Gameboard instance.
+   * @returns {undefined}
+   */
   const displayBoard = (gameboard) => {
     boardsContainer.appendChild(gameboard.board);
   }
 
+  /**
+   * Displays the draggable ships.
+   *
+   * @returns {undefined}
+   */
   const displayDraggables = () => {
+    /**
+     * Loop through ship information, create draggable elements.
+     * and append to container
+     * @param {string} name - The name of the ship.
+     * @param {number} length - The length of the ship.
+     */
     for (let [name, length] of Ship.shipInfo()) {
       const draggable = DOMBuilder.buildDraggable(name, length);
       draggablesContainer.appendChild(draggable)
     }
   }
 
+  /**
+   * Public API of the module.
+   * 
+   * @returns {Object} The public API with the displayBoard, displayDraggables functions.
+   */
   return { displayBoard, displayDraggables }
 })();
 
